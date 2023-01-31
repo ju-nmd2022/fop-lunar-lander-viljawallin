8f29188c6538e445ab45192352d4ae55a717e0ee
viljawallin <wavi22hy@student.ju.se>
2023-01-31 20:34:50 +0100

    The "start game" button is working!
    And I have started with the game-page

--- game.js
+++ game.js
@@ -2,52 +2,7 @@ let x = 100;
 let y = 100;
 let s = 0.8;
 
-function startScreen() {
-  background(235, 255, 230);
-  push();
-  noStroke();
-  fill(200, 180, 230);
-  rect(x - 100, y + 360, s * 10000);
-  pop();
-  bob(x + 140 * s, y, s * 1.2);
-  bubble(x + 135, y + 25, s * 1.15);
-}
-
-function gameScreen() {
-  background(255, 235, 230);
-  text("game on", 200, 100);
-}
-function resultScreen() {
-  background(225, 225, 255);
-  text("result", 200, 100);
-}
-
-let state = "start";
-let gameTimer = 0;
-
-function draw() {
-  if (state === "start") {
-    startScreen();
-  } else if (state === "game") {
-    gameScreen();
-    gameTimer = gameTimer + 1;
-    if (gameTimer >= 100) {
-      gameTimer = 0;
-      state = "result";
-    }
-  } else if (state === "result") {
-    resultScreen();
-  }
-}
-
-function mouseClicked() {
-  if (state === "start") {
-    state = "game";
-  } else if (state === "result") {
-    state = "game";
-  }
-}
-
+/* Bob the best */
 function bob(x, y, s) {
   /* Body */
   push();
@@ -138,7 +93,7 @@ function bob(x, y, s) {
 
 /*Talking bubble*/
 //Resource used to make this bubble= "Foundations of Programming - Emoji with a speechbubble", by Garrit.//
-function bubble(x, y, s) {
+function button(x, y, s) {
   push();
   beginShape();
   vertex(x + 150 * s, y - 20 * s);
@@ -160,16 +115,56 @@ function bubble(x, y, s) {
   pop();
 }
 
-bob(x, y, s * 1.2);
-bob(x + 200, y + 100, s);
+function startScreen() {
+  background(235, 255, 230);
+  push();
+  noStroke();
+  fill(200, 180, 230);
+  rect(x - 100, y + 360, s * 10000);
+  pop();
+  bob(x + 140 * s, y, s * 1.2);
+  button(x + 135, y + 25, s * 1.15);
+}
 
-// fill(255, 255, 255);
-// ellipse(x + 112, y + 6, 12);
-// ellipse(x + 177, y + 6, 12);
-// /* White pupil/light in the eyes */
+function gameScreen() {
+  background(255, 235, 230);
+  text("game on", 200, 100);
+}
+function resultScreen() {
+  background(225, 225, 255);
+  text("result", 200, 100);
+}
 
-// fill(200,40,40);
-// beginShape();
-// vertex(100, 290);
-// bezierVertex(100, 290, 110, 330, 125, 290);
-// endShape();
+let state = "start";
+let gameTimer = 0;
+let buttonIsClicked = false;
+
+function draw() {
+  if (state === "start") {
+    startScreen();
+  } else if (state === "game") {
+    gameScreen();
+    gameTimer = gameTimer + 1;
+    if (gameTimer >= 100) {
+      gameTimer = 0;
+      state = "result";
+    }
+  } else if (state === "result") {
+    resultScreen();
+  }
+}
+
+function mouseClicked() {
+  if (state === "start") {
+    if (
+      mouseX > x + 145 &&
+      mouseX < x + 145 + 245 &&
+      mouseY > y - 50 &&
+      mouseY < y + 10 + 5
+    ) {
+      state = "game";
+    }
+  } else if (state === "result") {
+    state = "game";
+  }
+}
